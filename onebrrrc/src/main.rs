aaa3547a96c6ae39b661f23c116bd4385bbf8a43
Arjun <11359226+yourarj@users.noreply.github.com>
2025-12-27 18:30:22 +0000

    (15% gain) number parsing logic
    Signed-off-by: Arjun <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sat Dec 27 18:30:22 2025 +0000

--- onebrrrc/src/main.rs
+++ onebrrrc/src/main.rs
@@ -11,28 +11,27 @@ fn main() {
     let f = fs::File::open("../measurements.txt").unwrap();
     let map = memmap(&f);
 
-    let mut stats = HashMap::<Vec<u8>, (f64, f64, usize, f64)>::new();
+    let mut stats = HashMap::<Vec<u8>, (i16, i64, usize, i16)>::new();
 
     for line in map.split(|c| *c == b'\n') {
         if line.is_empty() {
             break;
         }
+
         let mut fields = line.rsplitn(2, |c| *c == b';');
-        let temperature = fields.next().unwrap();
-        let temperature: f64 = unsafe { std::str::from_utf8_unchecked(temperature) }
-            .parse()
-            .unwrap();
+        let temperature = parse_temp(fields.next().unwrap());
+
         let station = fields.next().unwrap();
 
         let stats = match stats.get_mut(station) {
             Some(stats) => stats,
             None => stats
                 .entry(station.to_vec())
-                .or_insert((f64::MAX, 0., 0, f64::MIN)),
+                .or_insert((i16::MAX, 0, 0, i16::MIN)),
         };
 
         stats.0 = stats.0.min(temperature);
-        stats.1 += temperature;
+        stats.1 += i64::from(temperature);
         stats.2 += 1;
         stats.3 = stats.3.max(temperature);
     }
@@ -46,13 +45,43 @@ fn main() {
     let mut stats = stats.into_iter().peekable();
 
     while let Some((station, (min, sum, count, max))) = stats.next() {
-        print!("{station}={min:.1}/{:.1}/{max:.1}", sum / (*count as f64));
+        print!(
+            "{station}={:.1}/{:.1}/{:.1}",
+            (*min as f64) / 10.,
+            (*sum as f64) / 10. / (*count as f64),
+            (*max as f64) / 10.
+        );
 
         if stats.peek().is_some() {
             print!(", ")
         }
     }
     print!("}}");
+}
+
+/**
+ * parsing logic
+ */
+fn parse_temp(temp: &[u8]) -> i16 {
+    let mut t: i16 = 0;
+    let mut mul = 1;
+
+    for &d in temp.iter().rev() {
+        match d {
+            b'.' => {
+                continue;
+            }
+            b'-' => {
+                t = -t;
+                break;
+            }
+            _ => {
+                t += i16::from(d - b'0') * mul;
+                mul *= 10;
+            }
+        }
+    }
+    t
 }
 
 fn memmap(f: &File) -> &'_ [u8] {
